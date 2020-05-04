18e7f7434f06fde8b5812f39cfeb59097f4e9f51
chenkeju <505355520@qq.com>
2020-05-04 14:46:24 +0800

    2020/5/4

---
+++ 7流程控制.js
@@ -0,0 +1,24 @@
+/* 
+   顺序结构  自上而下执行
+
+   分支结构  根据不同的条件 执行不同的代码 得到不同的结果
+
+   循环结构  
+
+   三元表达式   
+            var num = 10
+            if(num>5) {
+                result = '是的'
+            } else {
+                result = '不是的'
+            }
+
+            var result = num > 5 ? '是的' : '不是的'
+            console.log(result);
+
+            案例2
+            var time = prompt('输入0-59的数字')
+            var result = time > 10 ? '0' + time : time
+            console.log(result);
+
+*/
---
+++ 8switch分支语句.js
@@ -0,0 +1,16 @@
+/* 
+    switch (条件表达式判断) {
+        case value1匹配表达式:
+            执行代码1;
+            break;
+        case value2:
+            执行代码2
+            break;
+        default:
+            执行语句
+    }
+
+    switch  对比 if else 效率高(直接跳到匹配值得语句执行) 书写代码多 判断条件适合特定值 不能是区间
+            case值 必须为常量
+            case值 和 变量表达式 必须全等匹配
+*/
